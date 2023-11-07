87c933d88d4187aecae77af68f592fd68a5b5eac
Brandon Trzepacz Tish <46979741+mackentish@users.noreply.github.com>
2023-11-07 12:50:35 -0500

    (WIP) install mysql

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import mysql from 'mysql';
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
@@ -6,6 +7,23 @@ import PocketsRouter from './pockets/pockets.router.config';
 import UsersRouter from './users/users.router.config';
 import GroupsRouter from './pocketGroups/groups.router.config';
 import TransactionsRouter from './transactions/transactions.router.config';
+
+// Connect to mysql database
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    port: (process.env.DB_PORT || 3306) as number,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'budge_it_DB',
+});
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+});
 
 // Connect to MongoDB database
 mongoose.connect('mongodb://localhost:27017/budge-it-DB').then(() => {
